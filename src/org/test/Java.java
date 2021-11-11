9878c69ac5df83ebad567b123d2a2554b809ebd8
git1111 <aartheegunasekar@gmail.com>
2021-11-12 00:08:55 +0530

    testbala

--- src/org/test/Java.java
+++ src/org/test/Java.java
@@ -4,6 +4,10 @@ public class Java {
 private void empname() {
 	System.out.println("Aarthi");
 }
+private void empadd() {
+	System.out.println("chennai");
+
+}
 public static void main(String[] args) {
 	Java a = new Java();
 	a.empname();
