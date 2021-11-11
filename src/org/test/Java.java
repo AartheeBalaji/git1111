e43470aae0b20eb4aba23dbced0342dc8743d696
git1111 <aartheegunasekar@gmail.com>
2021-11-12 00:04:56 +0530

    testaarthi

--- src/org/test/Java.java
+++ src/org/test/Java.java
@@ -4,6 +4,10 @@ public class Java {
 private void empname() {
 	System.out.println("Aarthi");
 }
+private void empid() {
+	System.out.println("1234");
+
+}
 public static void main(String[] args) {
 	Java a = new Java();
 	a.empname();
