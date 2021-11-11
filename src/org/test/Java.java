8f2021e23d224921b5be9c8646fd2ce4d07b8e77
git1111 <aartheegunasekar@gmail.com>
2021-11-11 23:54:42 +0530

    test

---
+++ src/org/test/Java.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Java {
+private void empname() {
+	System.out.println("Aarthi");
+}
+public static void main(String[] args) {
+	Java a = new Java();
+	a.empname();
+}
+
+}
